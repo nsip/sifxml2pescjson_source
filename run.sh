fca098a8bcd5f6c9b8c577d1f8ec6c07cf15fea2
Stuart McGrigor <Stuart.McGrigor@education.govt.nz>
2019-07-29 16:41:16 +1200

    Can now do the magic for AU or Infrastructure specs

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+
+#  Run ONE of the following to setup  objectgraph.txt and typegraph.txt
 #sh au_extract.sh
+#sh inf_extract.sh
 #sh input_extract.sh
 
 # 3. Generate transformation scripts and stylesheets
@@ -11,36 +14,53 @@ ruby makexslt.rb -p < scripts/out.txt > scripts/sif2jsonspecgen.xslt
 ruby makereorder.rb < scripts/out.txt > scripts/sifreorder.xslt
 ruby makejs2xml.rb < scripts/out.txt > scripts/json2sif.js
 
-
 # 4. Extract example XML from specgen
 mkdir -p test
 echo "<sif>" > test/siftest.xml 
-for filename in ./specgen_input/06_DataModel/Custom/Common/*.xml; do
-#for filename in ./specgen/GenerateSpecTool_5/bin/Debug/dist/Specification/06_DataModel/Custom/Common/*.xml; do
-  if [[ "$filename" == "./specgen_input/06_DataModel/Custom/Common/StudentScoreSet.xml" ]] || [[ "$filename" == "./specgen_input/06_DataModel/Custom/Common/PersonPrivacyObligation.xml" ]] || [[ "$filename" == "./specgen_input/06_DataModel/Custom/Common/ReportAuthorityInfo.xml" ]] ; then
-  #if [[ "$filename" == "./specgen/GenerateSpecTool_5/bin/Debug/dist/Specification/06_DataModel/Custom/Common/StudentScoreSet.xml" ]]; then
-    continue
-  fi
-  perl sifexamples.pl "$filename" >> test/siftest.xml
-done
-for filename in ./specgen_input/06_DataModel/Custom/AU/*.xml; do
-#for filename in ./specgen/GenerateSpecTool_5/bin/Debug/dist/Specification/06_DataModel/Custom/AU/*.xml; do
-  if [[ "$filename" == "./specgen_input/06_DataModel/Custom/Common/StudentScoreSet.xml" ]] || [[ "$filename" == "./specgen_input/06_DataModel/Custom/Common/PersonPrivacyObligation.xml" ]] || [[ "$filename" == "./specgen_input/06_DataModel/Custom/Common/ReportAuthorityInfo.xml" ]] ; then
-    continue
-  fi
-  perl sifexamples.pl "$filename" >> test/siftest.xml
-done
+
+if [ -d ./specgen_input/06_DataModel/Custom/Common ]; then
+  for filename in ./specgen_input/06_DataModel/Custom/Common/*.xml; do
+  #for filename in ./specgen/GenerateSpecTool_5/bin/Debug/dist/Specification/06_DataModel/Custom/Common/*.xml; do
+    if [[ "$filename" == "./specgen_input/06_DataModel/Custom/Common/StudentScoreSet.xml" ]] ||
+       [[ "$filename" == "./specgen_input/06_DataModel/Custom/Common/PersonPrivacyObligation.xml" ]] ||
+       [[ "$filename" == "./specgen_input/06_DataModel/Custom/Common/ReportAuthorityInfo.xml" ]] ; then
+      continue
+    fi
+    perl sifexamples.pl "$filename" >> test/siftest.xml
+  done
+fi
+
+if [ -d ./specgen_input/06_DataModel/Custom/AU ]; then
+  for filename in ./specgen_input/06_DataModel/Custom/AU/*.xml; do
+    perl sifexamples.pl "$filename" >> test/siftest.xml
+  done
+fi
+
+if [ -d ./specgen_input/06_DataModel/Custom/Infrastructure ]; then
+  for filename in ./specgen_input/06_DataModel/Custom/Infrastructure/*.xml; do
+
+    # exclude the two gCore dataobjects (for now)
+    # job.xml has xsi:type= definition which isn't handled when re-generating XML From JSON - so don't include in roundtrip tests
+    if [[ "$filename" == "./specgen_input/06_DataModel/Custom/Infrastructure/gCoreStaff.xml" ]] || 
+       [[ "$filename" == "./specgen_input/06_DataModel/Custom/Infrastructure/gCoreStudent.xml" ]] ||
+       [[ "$filename" == "./specgen_input/06_DataModel/Custom/Infrastructure/job.xml" ]]; then
+      continue
+    fi
+    perl sifexamples.pl "$filename" >> test/siftest.xml
+  done
+fi
+
 echo "</sif>" >> test/siftest.xml 
-xmllint --format test/siftest.xml > test/siftest.pretty.xml
+xmllint --c14n test/siftest.xml | xmllint --format - >test/siftest.pretty.xml
 
 # 5. Test roundtrip XML > JSON (preserving order of keys) > XML
 
 xsltproc scripts/sif2json.xslt test/siftest.xml > test/siftest.json
 jq . test/siftest.json > test/siftest.pretty.json
 echo "<sif>" > test/siftest2.xml
-node scripts/json2sif.js < test/siftest.pretty.json >> test/siftest2.xml
+bash -c "node scripts/json2sif.js < test/siftest.pretty.json >> test/siftest2.xml"
 echo "</sif>" >> test/siftest2.xml
-xmllint --format test/siftest2.xml > test/siftest2.pretty.xml
+xmllint --c14n test/siftest2.xml | xmllint --format - > test/siftest2.pretty.xml
 diff test/siftest.pretty.xml test/siftest2.pretty.xml > test/diff.txt
 cat test/diff.txt
 echo "Diff lines, roundtrip: "
@@ -54,7 +74,7 @@ echo "<sif>" > test/siftest3.xml
 node scripts/json2sif.js < test/siftest.sorted.json >> test/siftest3.xml
 echo "</sif>" >> test/siftest3.xml
 xsltproc scripts/sifreorder.xslt test/siftest3.xml > test/siftest.sorted.xml
-xmllint --format test/siftest.sorted.xml > test/siftest.sorted.pretty.xml
+xmllint --c14n test/siftest.sorted.xml | xmllint --format - > test/siftest.sorted.pretty.xml
 diff test/siftest2.pretty.xml test/siftest.sorted.pretty.xml > test/diff.sorted.txt
 cat test/diff.sorted.txt
 echo "Diff lines, re-sorting XML: "
