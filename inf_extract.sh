ba5e80a1f4a530b4a6923c5b313e0dd68af611a1
Nick Nicholas <opoudjis@gmail.com>
2023-01-19 13:06:49 +1100

    use current infrastructure source: https://github.com/nsip/specgen_input_au/issues/43

--- inf_extract.sh
+++ inf_extract.sh
@@ -7,8 +7,9 @@ shopt -s nocasematch
 
 
 # 1. Download specgen
-rm -fr DraftSIFInfrastructureSpec
-git clone https://github.com/nsip/DraftSIFInfrastructureSpec.git
+rm -fr specgen_input_infra
+#git clone https://github.com/nsip/DraftSIFInfrastructureSpec.git
+git clone https://github.com/Access4Learning/specgen_input_infra.git
 
 
 # 2. Extract all necessary information from specgen into flat files
@@ -22,10 +23,10 @@ containsElement () {
 }
 
 rm -f objs.txt
-xsltproc included_objects.xslt DraftSIFInfrastructureSpec/06_DataModel/Custom/DataModel-Infrastructure.xml | perl -ne 'next unless $_ =~ /\S/; next if $_ =~ /<\?/; s/^\s+//; s/\s+$//; print "./DraftSIFInfrastructureSpec/06_DataModel/Custom/" . $_ . "\n"' > objs.txt
+xsltproc included_objects.xslt specgen_input_infra/06_DataModel/Custom/DataModel-Infrastructure.xml | perl -ne 'next unless $_ =~ /\S/; next if $_ =~ /<\?/; s/^\s+//; s/\s+$//; print "./specgen_input_infra/06_DataModel/Custom/" . $_ . "\n"' > objs.txt
 IFS=$'\n' read -d '' -r -a objectarray < objs.txt
 
-for filename in ./DraftSIFInfrastructureSpec/06_DataModel/Custom/Infrastructure/*.xml; do
+for filename in ./specgen_input_infra/06_DataModel/Custom/Infrastructure/*.xml; do
   if containsElement "$filename" "${objectarray[@]}" ; then
     :
   else
@@ -40,8 +41,8 @@ done
 
 # process all common types in the specification
 echo '<root xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xi="http://www.w3.org/2001/XInclude" xmlns="http://sifassociation.org/SpecGen" xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns:xhtml="http://www.w3.org/1999/xhtml" >' > data.xml
-cat DraftSIFInfrastructureSpec/80_BackMatter/Generic-CommonTypes.xml >> data.xml
-cat DraftSIFInfrastructureSpec/80_BackMatter/Custom/DataModel-CommonTypes-Custom.xml >> data.xml
+cat specgen_input_infra/80_BackMatter/Generic-CommonTypes.xml >> data.xml
+cat specgen_input_infra/80_BackMatter/Custom/DataModel-CommonTypes-Custom.xml >> data.xml
 echo '</root>' >> data.xml
 xsltproc sifobject.xslt data.xml >> typegraph.txt
 
@@ -53,14 +54,14 @@ rm -f exp/*.xml
 echo "<sif>" > test/siftest.xml
 echo "<sif>" > test/siftest_specgen.xml
 
-if [ -d ./DraftSIFInfrastructureSpec/06_DataModel/Custom/Infrastructure ]; then
-  for filename in ./DraftSIFInfrastructureSpec/06_DataModel/Custom/Infrastructure/*.xml; do
+if [ -d ./specgen_input_infra/06_DataModel/Custom/Infrastructure ]; then
+  for filename in ./specgen_input_infra/06_DataModel/Custom/Infrastructure/*.xml; do
 
     # exclude the two gCore dataobjects (for now)
     # job.xml has xsi:type= definition which isn't handled when re-generating XML From JSON - so don't include in roundtrip tests
-    if [[ "$filename" == "./DraftSIFInfrastructureSpec/06_DataModel/Custom/Infrastructure/gCoreStaff.xml" ]] ||
-       [[ "$filename" == "./DraftSIFInfrastructureSpec/06_DataModel/Custom/Infrastructure/gCoreStudent.xml" ]] ||
-       [[ "$filename" == "./DraftSIFInfrastructureSpec/06_DataModel/Custom/Infrastructure/job.xml" ]]; then
+    if [[ "$filename" == "./specgen_input_infra/06_DataModel/Custom/Infrastructure/gCoreStaff.xml" ]] ||
+       [[ "$filename" == "./specgen_input_infra/06_DataModel/Custom/Infrastructure/gCoreStudent.xml" ]] ||
+       [[ "$filename" == "./specgen_input_infra/06_DataModel/Custom/Infrastructure/job.xml" ]]; then
       continue
     fi
     perl sifexamples.pl "$filename" >> test/siftest.xml
@@ -68,10 +69,10 @@ if [ -d ./DraftSIFInfrastructureSpec/06_DataModel/Custom/Infrastructure ]; then
   done
 fi
 
-perl sifexamples.pl ./DraftSIFInfrastructureSpec/80_BackMatter/Generic-CommonTypes.xml >> test/siftest_specgen.xml
-perl sifexamples.pl ./DraftSIFInfrastructureSpec/80_BackMatter/Custom/DataModel-CommonTypes-Custom.xml >> test/siftest_specgen.xml
-#perl sifexamples1.pl ./DraftSIFInfrastructureSpec/80_BackMatter/Generic-CommonTypes.xml exp
-#perl sifexamples1.pl ./DraftSIFInfrastructureSpec/80_BackMatter/Custom/DataModel-CommonTypes-Custom.xml exp
+perl sifexamples.pl ./specgen_input_infra/80_BackMatter/Generic-CommonTypes.xml >> test/siftest_specgen.xml
+perl sifexamples.pl ./specgen_input_infra/80_BackMatter/Custom/DataModel-CommonTypes-Custom.xml >> test/siftest_specgen.xml
+#perl sifexamples1.pl ./specgen_input_infra/80_BackMatter/Generic-CommonTypes.xml exp
+#perl sifexamples1.pl ./specgen_input_infra/80_BackMatter/Custom/DataModel-CommonTypes-Custom.xml exp
 
 #cat attribute_test.xml >> test/siftest.xml
 
@@ -79,5 +80,5 @@ echo "</sif>" >> test/siftest.xml
 echo "</sif>" >> test/siftest_specgen.xml
 xmllint --c14n test/siftest.xml | xmllint --format - >test/siftest.pretty.xml
 
-grep "schemaVersion" specgen_input_au/SIF.Config_DataModel_IN.xml | perl -pe 's/\s*<schemaVersion>/VERSION: /; s#</schemaVersion>##; $$_;' > scripts/out.txt
+grep "schemaVersion" specgen_input_infra/SIF.Config_DataModel_IN.xml | perl -pe 's/\s*<schemaVersion>/VERSION: /; s#</schemaVersion>##; $$_;' > scripts/out.txt
 
